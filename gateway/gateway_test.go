297953d6aacacc03554d54be8e6e77e891ae9dd1
Parham Alvani <parham.alvani@gmail.com>
2018-02-11 06:37:15 -0500

    Adds gateway test.

---
+++ gateway/gateway_test.go
@@ -0,0 +1,29 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 06-02-2018
+ * |
+ * | File Name:     runner/runner_test.go
+ * +===============================================
+ */
+
+package runner
+
+import "testing"
+
+func TestBasic(t *testing.T) {
+	r, err := New("Eli", "127.0.0.1:1883")
+
+	if err != nil {
+		t.Fatalf("Runner creation error: %s", err)
+	}
+
+	t.Log(r.ID)
+
+	err = r.Remove()
+
+	if err != nil {
+		t.Fatalf("Runner remove error: %s", err)
+	}
+}
